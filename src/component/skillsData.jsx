f1c026a576916ecc5ea619f6ceb75bc86db13c58
iaidris <idrismutolib5@gmail.com>
2024-05-26 00:15:17 +0100

    live demo links added

--- src/component/skillsData.jsx
+++ src/component/skillsData.jsx
@@ -3,7 +3,8 @@ const projects= [
     id:1,
     description: 'A website for a gym or fitness center that provides information about membership plans, class schedules, trainer profiles, and facilities.',
    technologies: [ 'CSS', 'JavaScript', 'React'],
-    demo:"",
+    demo:"afims-fitness-gym.vercel.app",
+    
     github:"",
     image:"images/gym2.jpg",
 
@@ -15,7 +16,7 @@ const projects= [
 
     description: "An online store for selling products or services. It includes product listings with images, descriptions, prices, and customer reviews.",
    technologies: [ 'JavaScript', 'React', 'tailwind'],
-   demo:"",
+   demo:"anabe-ecommerce.vercel.app",
    github:"",
    image:"images/cart1.jpg"
   },
@@ -24,7 +25,7 @@ const projects= [
   
     description: " A platform for buying, selling, and renting properties. It typically includes property listings with images, descriptions, prices, and contact information for estate agents.",
    technologies: [ 'JavaScript', 'React', 'tailwind'],
-   demo:"",
+   demo:"kaison-real-estate.vercel.app",
    github:"",
    image:"images/propertybg.jpg"
   },
@@ -33,7 +34,7 @@ const projects= [
     
     description: " An online presence for a barbershop that showcases services, pricing, barber profiles & apppointment booking. ",
    technologies: ['JavaScript', 'React', 'tailwind'],
-   demo:"",
+   demo:"prime-barbershop.vercel.app",
    github:"",
    image:"images/barber1.jpg",
   }
